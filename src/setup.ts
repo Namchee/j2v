4a4acde452ae6f538dc5e025ece37e2a839d7caa
Cristopher <cristophernamchee12@gmail.com>
2024-07-08 21:51:13 +0700

    feat: add cleanup file generator

--- src/setup.ts
+++ src/setup.ts
@@ -1,13 +1,21 @@
 import type { UserConfig } from "vitest/config";
 
-export function addCleanupFiles(config: UserConfig, packages: string[]) {
-  const vitestImport = [];
+export type CleanupFile = {
+  plugins: string[];
+  vitestImports: string[];
+  imports: string[];
+  code: string[];
+}
+
+export function addCleanupFiles(config: UserConfig["test"], packages: string[]): CleanupFile | undefined {
+  const vitestImports = [];
   const imports = [];
   const code: string[] = [];
+  const plugins: string[] = [];
 
-  const isDom = ['happy-dom', 'jsdom'].includes(config?.test?.environment || '');
+  const isDom = ['happy-dom', 'jsdom'].includes(config?.environment || '');
   if (!isDom) {
-    return;
+    return undefined;
   }
 
   const hasJestDOMInstalled = packages.includes('@testing-library/jest-dom');
@@ -32,8 +40,22 @@ export function addCleanupFiles(config: UserConfig, packages: string[]) {
     imports.push(`import { cleanup } from '@testing-library/preact';`)
   }
 
+  if (hasSvelteInstalled) {
+    const isSveltekit = packages.includes('@sveltejs/kit');
+    if (isSveltekit) {
+      imports.push(`import { sveltekit } from '@sveltejs/kit/vite';`);
+      plugins.push('sveltekit()');
+    } else {
+      imports.push(`import { svelte } from '@sveltejs/vite-plugin-svelte';`);
+      plugins.push('svelte()');
+    }
+
+    imports.push(`import { svelteTesting } from '@testing-library/svelte/vite';`);
+    plugins.push('svelteTesting()');
+  }
+
   if (hasReactInstalled || hasVueInstalled || hasSvelteInstalled || hasPreactInstalled) {
-    vitestImport.push('afterEach');
+    vitestImports.push('afterEach');
 
     if (code) {
       code.push('\n');
@@ -43,5 +65,12 @@ export function addCleanupFiles(config: UserConfig, packages: string[]) {
   cleanup();
 });`);
   }
+
+  return {
+    vitestImports,
+    imports,
+    code,
+    plugins,
+  };
 }
 
