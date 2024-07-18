8f41adfdeda5b3086ea7f2e2fd7d9d3a51eb5d4b
Cristopher <cristophernamchee12@gmail.com>
2024-07-18 21:37:13 +0700

    feat: register CLI map list

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,7 +1,41 @@
 
+// Maps Jest CLI options to Vitest
 const JEST_CLI_MAP = {
-
-}
+  bail: ['bail'],
+  changedFilesWithAncestor: ['changed', 'HEAD~1'],
+  changedSince: ['changed'],
+  config: ['config'],
+  c: ['c'],
+  coverage: ['coverage.enabled'],
+  collectCoverage: ['coverage.enabled'],
+  coverageDirectory: ['coverage.reportsDirectory'],
+  env: ['environment'],
+  expand: ['expandSnapshotDiff'],
+  injectGlobals: ['globals'],
+  json: ['json'],
+  lastCommit: ['changed', 'HEAD~1'],
+  logHeapUsage: ['logHeapUsage'],
+  maxConcurrency: ['maxConcurrency'],
+  maxWorkers: ['maxWorkers'],
+  noStackTrace: ['printConsoleTrace', 'false'],
+  onlyChanged: ['changed', 'HEAD~1'],
+  outputFile: ['outputFile'],
+  passWithNoTests: ['passWithNoTests'],
+  randomize: ['sequence.shuffle.tests'],
+  seed: ['sequence.seed'],
+  reporters: ['coverage.reporter'],
+  roots: ['root'], // TODO: multi value
+  runInBand: ['sequence.concurrent', 'false'],
+  selectProjects: ['project'], // TODO: multi value
+  shard: ['shard'],
+  silent: ['silent'],
+  testNamePattern: ['testNamePattern'],
+  t: ['t'],
+  testPathIgnorePatterns: ['exclude'],
+  testTimeout: ['testTimeout'],
+  updateSnaphot: ['update'],
+  watch: ['watch']
+};
 
 function isJestCommand(command: string) {
   return command.match(/^.+?jest/);
