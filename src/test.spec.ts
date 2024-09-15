e727cb19e444a579faedcc65f8812a3bb6eadc92
Cristopher <cristophernamchee12@gmail.com>
2024-09-15 22:22:40 +0700

    test: write base test for test-file reader

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -1,5 +1,15 @@
 import { describe, } from "vitest";
 
 describe("getTestFiles", () => {
+  it("should process inclusion and exclusion from config", () => {
 
+  });
+
+  it("should use default inclusion", () => {
+
+  });
+
+  it("should use default exclusion", () => {
+
+  })
 });
