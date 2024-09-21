0c938b1024284dab0faeb0fae87396cce8253890
Cristopher <cristophernamchee12@gmail.com>
2024-09-21 21:52:58 +0700

    docs: rewrite documentation

--- README.md
+++ README.md
@@ -1,22 +1,56 @@
 # j2v
 
-Jest to Vitest CLI migration tool
+All-in-one Jest to Vitest migration tool.
 
-## Roadmap
+## Features
 
-- [x] Jest Config Migration
-  - [x] Configuration Converter
-- [x] Jest Setup file migration and auto setup for JSDOM
-  - [x] React
-  - [x] Vue
-  - [x] Svelte
-  - [x] Angular
-- [x] Script Migration
-  - [x] Create new script
-  - [x] Mod the existing one
-- [x] Dependency Migration
-- [x] CLI
-- [x] Test file converter
+- JavaScript / TypeScript compatible
+- Automatically generates Vitest configuration that mimics your Jest configuration as close as possible
+- Setup file generation for automated code cleanups
+- Automatic Jest dependency cleanup
+
+## Usage
+
+```bash
+## If you prefer using it without explicit installation:
+# Using npm
+npx @namchee/j2v
+
+# Using pnpm
+pnpx @namchee/j2v
+
+# Using bun
+bunx @namchee/j2v
+
+## Or... install the package
+# Using npm
+npm install -D @namchee/j2v
+
+# Using yarn
+yarn add -D @namchee/j2v
+
+# Using pnpm
+pnpm install -D @namchee/j2v
+
+# Using bun
+bun add -D @namchee/j2v
+```
+
+## Commands
+
+```bash
+j2v/0.1.0
+
+Usage:
+  $ j2v <command> [options]
+
+Options:
+  -h, --help     Display this message
+  -v, --version  Display version number
+  --globals      Enable Vitest global API to your test files (default: false)
+  -d, --debug    Show debugging output (default: false)
+  --dry-run      Perform a dry-run of the CLI (default: false)
+```
 
 ## Compatibility
 
@@ -24,17 +58,16 @@ Not all of Jest APIs and quirks will be replaced by this utility, as some of the
 
 For the list of all compatible APIs, please see the [compatibility list](./COMPATIBILITY.md)
 
-## Caveats
+## Notes
 
-1. Currently, the code will be formatted with the following style:
-  - Two spaces indentation
-  - LF end of line
-  - Comma dangle
+1. Jest configuration will be evaluated during conversion. Expect some derivative values (from variables, functions, etc) to be transformed into raw values.
+2. Code style for transformed such as dangling commas, brace spacing on destructured objects, and semicolons might not follow the original style of the code.
+3. Setup file written in CommonJS will break the test
 
 ## Acknowledgments
 
 1. This project would not exist without the existence of this [Jest migration to Vitest gist](https://gist.github.com/wojtekmaj/6defa1f358daae28bd52b7b6dbeb7ab6)
-2. [vitest-codemod](https://github.com/trivikr/vitest-codemod)
+2. [vitest-codemod](https://github.com/trivikr/vitest-codemod) as further inspiration on how should the CLI behave
 
 ## License
 
--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,12 @@ import color from "picocolors";
 
 import { Logger } from "./logger";
 
-import { getNeededPackages, getRemovedPackages, install, uninstall } from "./deps";
+import {
+  getNeededPackages,
+  getRemovedPackages,
+  install,
+  uninstall,
+} from "./deps";
 import { formatSetupFile, formatVitestConfig } from "./formatter";
 import { getJestConfig } from "./jest";
 import { transformJestConfigToVitest } from "./mapper";
@@ -21,7 +26,9 @@ import { transformJestScriptsToVitest } from "./scripts";
 import { type CleanupFile, constructDOMCleanupFile } from "./setup";
 import { transformJestTestToVitest } from "./transformer";
 
-const cli = cac();
+const cli = cac("j2v");
+cli.help();
+cli.version("0.1.0");
 cli
   .option("--globals", "Enable Vitest global API to your test files", {
     default: false,
@@ -34,214 +41,221 @@ cli
   });
 
 const args = cli.parse();
-const spinner = ora(color.green("Finding Jest config...\n"));
 
-Logger.init(args.options.debug);
+if (!args.options.help) {
+  const spinner = ora(color.green("Finding Jest config...\n"));
 
-try {
-  spinner.start();
+  Logger.init(args.options.debug);
 
-  const isTS = existsSync(resolve(process.cwd(), "tsconfig.json"));
+  try {
+    spinner.start();
 
-  const { path, config } = await getJestConfig();
+    const isTS = existsSync(resolve(process.cwd(), "tsconfig.json"));
 
-  Logger.debug(
-    path.length
-      ? `Using Jest configuration from ${path}`
-      : "Jest configuration not found. Using default configuration values.",
-  );
-
-  spinner.text = color.green(
-    "Configuring Vitest based on Jest configuration...\n",
-  );
-
-  const vitestConfig = transformJestConfigToVitest(
-    config,
-    args.options.globals,
-  );
-
-  let setupFile: CleanupFile | undefined;
-
-  Logger.debug("Vitest configuration generated");
-
-  spinner.text = color.green("Scanning directory for test files...\n");
-
-  const testFiles = await getTestFiles(vitestConfig);
-
-  Logger.debug(`${testFiles.length} test files found.`);
-
-  const packageJsonPath = resolve(process.cwd(), "package.json");
-  let scripts: string[] = [];
-  let installed: string[] = [];
-  let uninstalled: string[] = [];
-
-  if (existsSync(packageJsonPath)) {
-    Logger.debug("package.json found. Jest scripts will be transformed.");
-
-    spinner.text = color.green("Transforming package's scripts...\n");
-
-    const packageJson = JSON.parse(readFileSync(packageJsonPath).toString());
-    const dependencies = [
-      ...Object.keys(packageJson.dependencies),
-      ...Object.keys(packageJson.devDependencies),
-    ];
-
-    const scriptData = transformJestScriptsToVitest(packageJson.scripts);
-    scripts = scriptData.modified;
-    setupFile = constructDOMCleanupFile(vitestConfig, dependencies);
-
-    installed = getNeededPackages(dependencies, scriptData, vitestConfig);
-    uninstalled = getRemovedPackages(dependencies);
-  } else {
-    Logger.info(
-      "package.json not found, skipping scripts transformation and dependency cleanup.",
-    );
-  }
-
-  const configFilename = resolve(
-    process.cwd(),
-    `vitest.config.${isTS ? "ts" : "js"}`,
-  );
-  const setupFilename = resolve(
-    process.cwd(),
-    `vitest.setup.${isTS ? "ts" : "js"}`,
-  );
-
-  const report = [];
-
-  let header = args.options.dryRun
-    ? `Found ${testFiles.length} test file(s) that can be transformed${testFiles.length ? ":" : "."}`
-    : `Transformed ${testFiles.length} test file(s)${testFiles.length ? ":" : "."}`;
-
-  if (testFiles.length) {
-    header += `\n${testFiles.map((test) => `  • ${basename(test.path)} ➜ ${test.path}`).join("\n")}`;
-  }
-
-  report.push(header);
-
-  if (installed.length) {
-    const list = installed.map((dep) => `  • ${dep}`).join("\n");
-    report.push(
-      args.options.dryRun
-        ? `Dependencies that will be installed:\n${list}`
-        : `Installed dependencies:\n${list}`,
-    );
-  }
-
-  if (uninstalled.length) {
-    const list = uninstalled.map((dep) => `  • ${dep}`).join("\n");
-    report.push(
-      args.options.dryRun
-        ? `Dependencies that will be removed:\n${list}`
-        : `Removed dependencies:\n${list}`,
-    );
-  }
-
-  if (scripts.length) {
-    const list = scripts.map((dep) => `  • ${dep}`).join("\n");
-    report.push(
-      args.options.dryRun
-        ? `NPM scripts that will be transformed / introduced:\n${list}`
-        : `Transformed / introduced npm scripts:\n${list}`,
-    );
-  }
-
-  const createdFiles = [`  • ${basename(configFilename)}`];
-  if (setupFile) {
-    createdFiles.push(`  • ${basename(setupFilename)}`);
-  }
-  report.push(
-    args.options.dryRun
-      ? `File(s) that will be created:\n${createdFiles.join("\n")}`
-      : `Created file(s):\n${createdFiles.join("\n")}`,
-  );
-
-  if (path) {
-    report.push(
-      args.options.dryRun
-        ? `File(s) that will be deleted:\n  • ${basename(path)}`
-        : `Deleted file(s):\n  • ${basename(path)}`,
-    );
-  }
-
-  if (args.options.dryRun) {
-    spinner.succeed(" Dry-run completed successfully.\n");
-
-    Logger.info(report.join("\n\n"));
-  } else {
-    spinner.text = color.green("Writing Vitest config (and setup files)...\n");
-
-    if (setupFile) {
-      const setupText = formatSetupFile(setupFile);
-
-      if (Array.isArray(vitestConfig?.setupFiles)) {
-        vitestConfig.setupFiles.push(`./${setupFilename}`);
-      }
-
-      const setupFilepath = resolve(process.cwd(), setupFilename);
-
-      writeFileSync(setupFilepath, setupText);
-
-      Logger.debug(
-        `Successfully written Vitest setup file on ${setupFilepath}`,
-      );
-    } else {
-      Logger.debug(
-        "Setup file is not necessary as target environment is not DOM.",
-      );
-    }
-
-    const configText = formatVitestConfig(vitestConfig);
-
-    writeFileSync(configFilename, configText);
+    const { path, config } = await getJestConfig();
 
     Logger.debug(
-      `Successfully written Vitest configuration file on ${configFilename}`,
+      path.length
+        ? `Using Jest configuration from ${path}`
+        : "Jest configuration not found. Using default configuration values.",
     );
 
     spinner.text = color.green(
-      `Transforming ${testFiles.length} test file(s)...\n`,
+      "Configuring Vitest based on Jest configuration...\n",
     );
 
-    const transformedTests = transformJestTestToVitest(
-      testFiles,
+    const vitestConfig = transformJestConfigToVitest(
+      config,
       args.options.globals,
     );
 
-    await Promise.all(
-      transformedTests.map((test) => writeFile(test.path, test.content)),
-    );
+    let setupFile: CleanupFile | undefined;
 
-    Logger.debug(`Succesfully transformed ${testFiles.length} test file(s)`);
+    Logger.debug("Vitest configuration generated");
 
-    spinner.text = color.green("Tidying package dependencies...");
+    spinner.text = color.green("Scanning directory for test files...\n");
 
-    const pm = await detect();
-    install(pm, installed);
-    uninstall(pm, uninstalled);
+    const testFiles = await getTestFiles(vitestConfig);
 
-    Logger.debug("Succesfully installed required dependencies and removed unnecessary dependencies");
+    Logger.debug(`${testFiles.length} test files found.`);
 
-    if (path) {
-      spinner.text = "Removing unnecessary file(s)..."
+    const packageJsonPath = resolve(process.cwd(), "package.json");
+    let scripts: string[] = [];
+    let installed: string[] = [];
+    let uninstalled: string[] = [];
 
-      rmSync(path);
+    if (existsSync(packageJsonPath)) {
+      Logger.debug("package.json found. Jest scripts will be transformed.");
 
-      Logger.debug("Succesfully removed unnecessary files");
+      spinner.text = color.green("Transforming package's scripts...\n");
+
+      const packageJson = JSON.parse(readFileSync(packageJsonPath).toString());
+      const dependencies = [
+        ...Object.keys(packageJson.dependencies),
+        ...Object.keys(packageJson.devDependencies),
+      ];
+
+      const scriptData = transformJestScriptsToVitest(packageJson.scripts);
+      scripts = scriptData.modified;
+      setupFile = constructDOMCleanupFile(vitestConfig, dependencies);
+
+      installed = getNeededPackages(dependencies, scriptData, vitestConfig);
+      uninstalled = getRemovedPackages(dependencies);
+    } else {
+      Logger.info(
+        "package.json not found, skipping scripts transformation and dependency cleanup.",
+      );
     }
 
-    Logger.info(report.join("\n\n"));
-
-    spinner.succeed(
-      color.green(
-        `✨ Succesfully converted Jest test suite to Vitest. You're good to Vitest 🚀\n`,
-      ),
+    const configFilename = resolve(
+      process.cwd(),
+      `vitest.config.${isTS ? "ts" : "js"}`,
     );
+    const setupFilename = resolve(
+      process.cwd(),
+      `vitest.setup.${isTS ? "ts" : "js"}`,
+    );
+
+    const report = [];
+
+    let header = args.options.dryRun
+      ? `Found ${testFiles.length} test file(s) that can be transformed${testFiles.length ? ":" : "."}`
+      : `Transformed ${testFiles.length} test file(s)${testFiles.length ? ":" : "."}`;
+
+    if (testFiles.length) {
+      header += `\n${testFiles.map((test) => `  • ${basename(test.path)} ➜ ${test.path}`).join("\n")}`;
+    }
+
+    report.push(header);
+
+    if (installed.length) {
+      const list = installed.map((dep) => `  • ${dep}`).join("\n");
+      report.push(
+        args.options.dryRun
+          ? `Dependencies that will be installed:\n${list}`
+          : `Installed dependencies:\n${list}`,
+      );
+    }
+
+    if (uninstalled.length) {
+      const list = uninstalled.map((dep) => `  • ${dep}`).join("\n");
+      report.push(
+        args.options.dryRun
+          ? `Dependencies that will be removed:\n${list}`
+          : `Removed dependencies:\n${list}`,
+      );
+    }
+
+    if (scripts.length) {
+      const list = scripts.map((dep) => `  • ${dep}`).join("\n");
+      report.push(
+        args.options.dryRun
+          ? `NPM scripts that will be transformed / introduced:\n${list}`
+          : `Transformed / introduced npm scripts:\n${list}`,
+      );
+    }
+
+    const createdFiles = [`  • ${basename(configFilename)}`];
+    if (setupFile) {
+      createdFiles.push(`  • ${basename(setupFilename)}`);
+    }
+    report.push(
+      args.options.dryRun
+        ? `File(s) that will be created:\n${createdFiles.join("\n")}`
+        : `Created file(s):\n${createdFiles.join("\n")}`,
+    );
+
+    if (path) {
+      report.push(
+        args.options.dryRun
+          ? `File(s) that will be deleted:\n  • ${basename(path)}`
+          : `Deleted file(s):\n  • ${basename(path)}`,
+      );
+    }
+
+    if (args.options.dryRun) {
+      spinner.succeed(" Dry-run completed successfully.\n");
+
+      Logger.info(report.join("\n\n"));
+    } else {
+      spinner.text = color.green(
+        "Writing Vitest config (and setup files)...\n",
+      );
+
+      if (setupFile) {
+        const setupText = formatSetupFile(setupFile);
+
+        if (Array.isArray(vitestConfig?.setupFiles)) {
+          vitestConfig.setupFiles.push(`./${setupFilename}`);
+        }
+
+        const setupFilepath = resolve(process.cwd(), setupFilename);
+
+        writeFileSync(setupFilepath, setupText);
+
+        Logger.debug(
+          `Successfully written Vitest setup file on ${setupFilepath}`,
+        );
+      } else {
+        Logger.debug(
+          "Setup file is not necessary as target environment is not DOM.",
+        );
+      }
+
+      const configText = formatVitestConfig(vitestConfig);
+
+      writeFileSync(configFilename, configText);
+
+      Logger.debug(
+        `Successfully written Vitest configuration file on ${configFilename}`,
+      );
+
+      spinner.text = color.green(
+        `Transforming ${testFiles.length} test file(s)...\n`,
+      );
+
+      const transformedTests = transformJestTestToVitest(
+        testFiles,
+        args.options.globals,
+      );
+
+      await Promise.all(
+        transformedTests.map((test) => writeFile(test.path, test.content)),
+      );
+
+      Logger.debug(`Succesfully transformed ${testFiles.length} test file(s)`);
+
+      spinner.text = color.green("Tidying package dependencies...");
+
+      const pm = await detect();
+      install(pm, installed);
+      uninstall(pm, uninstalled);
+
+      Logger.debug(
+        "Succesfully installed required dependencies and removed unnecessary dependencies",
+      );
+
+      if (path) {
+        spinner.text = "Removing unnecessary file(s)...";
+
+        rmSync(path);
+
+        Logger.debug("Succesfully removed unnecessary files");
+      }
+
+      Logger.info(report.join("\n\n"));
+
+      spinner.succeed(
+        color.green(
+          `✨ Succesfully converted Jest test suite to Vitest. You're good to Vitest 🚀\n`,
+        ),
+      );
+    }
+  } catch (err) {
+    const error = err as Error;
+
+    spinner.fail(color.red(`Transformation failed due to ${error.message}\n`));
+
+    Logger.error(error);
   }
-} catch (err) {
-  const error = err as Error;
-
-  spinner.fail(color.red(`❌ Transformation failed due to ${error.message}\n`));
-
-  Logger.error(error);
 }
