64df5fdfc1097ae66c9895ec294f0c91f834e9a2
Cristopher <cristophernamchee12@gmail.com>
2024-09-22 22:22:23 +0700

    test: add 'globals' and multi-file test

---
+++ fixtures/globals.in.ts
@@ -0,0 +1,14 @@
+describe("Sample test", () => {
+  it("return 2", () => {
+    const result = 1 + 1;
+
+    expect(result).toBe(2);
+  });
+
+  it("should return `true`", () => {
+    const result = 1;
+    const assertion = result === 1;
+
+    expect(assertion).toBe(true);
+  });
+});
--- src/transformer.test.ts
+++ src/transformer.test.ts
@@ -12,7 +12,7 @@ const readFile = promisify(readFileCb);
 
 describe("transformJestToVitest", () => {
   const givenInput = globSync(["fixtures/**/*.in.ts"], {
-    ignore: ["fixtures/global.(in|out).(j|t)sx?"],
+    ignore: ["fixtures/globals.in.ts"],
   });
 
   beforeAll(() => {
@@ -41,4 +41,52 @@ describe("transformJestToVitest", () => {
       expect(transformed[0]?.content).toBe(inputContent[1].toString());
     });
   }
+
+  it("should not import anything if 'globals' is enabled", async () => {
+    const inputContent = await Promise.all([
+      readFile(`${process.cwd()}/fixtures/globals.in.ts`),
+      readFile(`${process.cwd()}/fixtures/globals.out.ts`),
+    ]);
+
+    const transformed = transformJestTestToVitest(
+      [
+        {
+          path: "fixtures/globals.in.ts",
+          content: inputContent[0].toString(),
+        },
+      ],
+      {
+        globals: true
+      },
+    );
+
+    expect(transformed[0]?.content).toBe(inputContent[1].toString());
+  });
+
+  it("should transform multiple files at the same time", async () => {
+    const inputContent = await Promise.all([
+      readFile(`${process.cwd()}/fixtures/afterAll.in.ts`),
+      readFile(`${process.cwd()}/fixtures/afterAll.out.ts`),
+      readFile(`${process.cwd()}/fixtures/beforeAll.in.ts`),
+      readFile(`${process.cwd()}/fixtures/beforeAll.out.ts`),
+    ]);
+
+    const transformed = transformJestTestToVitest(
+      [
+        {
+          path: "fixtures/afterAll.in.ts",
+          content: inputContent[0].toString(),
+        },
+        {
+          path: "fixtures/beforeAll.in.ts",
+          content: inputContent[2].toString(),
+        },
+      ],
+      {
+      },
+    );
+
+    expect(transformed[0]?.content).toBe(inputContent[1].toString());
+    expect(transformed[1]?.content).toBe(inputContent[3].toString());
+  });
 });
